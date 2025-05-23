fe99acc0e5e3b9305b68395cb4b9aade86412c46
Dikshit Mahanot <168990083+dikjain@users.noreply.github.com>
2025-05-23 19:46:49 +0530

    done

--- middleware.prod.ts
+++ middleware.prod.ts
@@ -21,7 +21,7 @@ export default function middleware(request: NextRequest) {
     'Content-Security-Policy',
     `
       default-src 'self';
-      script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https://securetoken.googleapis.com https://dev.userology.co https://www.figma.com https://embed.figma.com https://identitytoolkit.googleapis.com https://*.userology.co https://*.userologyai.com https://*.userology.info https://cdn.mxpnl.com;
+      script-src 'self' 'nonce-${nonce}' https://securetoken.googleapis.com https://dev.userology.co https://www.figma.com https://embed.figma.com https://identitytoolkit.googleapis.com https://*.userology.co https://*.userologyai.com https://*.userology.info https://cdn.mxpnl.com;
       style-src 'self' 'nonce-${nonce}' https://fonts.googleapis.com;
       img-src 'self' data: blob: https: https://userology-figma.s3.us-west-2.amazonaws.com;
       font-src 'self' https://fonts.gstatic.com;
@@ -29,7 +29,7 @@ export default function middleware(request: NextRequest) {
       base-uri 'self';
       form-action 'self';
       frame-ancestors 'none';
-      connect-src 'self' https://securetoken.googleapis.com https://dev.userology.co https://dev.userologyai.com https://userology.info https://userologyai.com https://userology.co wss://*.userology.co https://www.figma.com https://embed.figma.com https://userology-figma.s3.us-west-2.amazonaws.com https://identitytoolkit.googleapis.com https://*.userologyai.com https://*.userology.* https://dev.userology.info https://*.userology.co https://*.userology.info https://api.mixpanel.com https://*.mxpnl.com https://api-js.mixpanel.com;
+      connect-src 'self' https://securetoken.googleapis.com https://dev.userology.co https://dev.userologyai.com https://userology.info https://userologyai.com https://userology.co wss://*.userology.co https://www.figma.com https://embed.figma.com https://userology-figma.s3.us-west-2.amazonaws.com https://identitytoolkit.googleapis.com https://*.userologyai.com https://dev.userology.info https://*.userology.co https://*.userology.info https://api.mixpanel.com https://*.mxpnl.com https://api-js.mixpanel.com;
       media-src 'self';
       worker-src 'self';
       manifest-src 'self';
