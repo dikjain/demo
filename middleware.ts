85417dc738a3387ec12f8d1871cd8298b9de0333
Dikshit Mahanot <168990083+dikjain@users.noreply.github.com>
2025-05-23 20:07:30 +0530

    fix

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ export function middleware(request: NextRequest) {
   const cspHeader = `
     default-src 'self';
     script-src 'self' 'nonce-${nonce}' 'unsafe-eval';
-    script-src-elem 'self' 'nonce-${nonce}' 'unsafe-inline';
+    script-src-elem 'self' 'nonce-${nonce}';
     style-src 'self' 'nonce-${nonce}';
     img-src 'self' blob: data: https://userology-figma.s3.us-west-2.amazonaws.com;
     font-src 'self' https://fonts.gstatic.com;
